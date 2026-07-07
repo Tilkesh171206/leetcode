cb061f572871f9544022a6d845caaad5a9cb12c5
Tilkesh Tailor <tilkeshtailor46@gmail.com>
2026-07-08 03:39:09 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.91%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,12 +3,12 @@ public:
     int reverse(int x) {
         long long n=0;
         bool sign=false;
-        if(x<0){bool sign=true;}
+        if(x<0){ sign=true;}
         while(x!=0){
             n=n*10+x%10;
             x=x/10;
         }
-        if(sign){n=-n;}
+        // if(sign){n=-n;}
         if(n>INT_MAX || n<INT_MIN){return 0;}
         return (int)n;
     }
